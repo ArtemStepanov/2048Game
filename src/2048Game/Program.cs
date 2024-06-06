1732a0faf0b2a404aff75b09b029aebbdf1d7de8
Artem <stxima@stxima.com>
2024-06-07 01:07:49 +0300

    Update Qodana CI workflow and add code coverage info. Refactor Program.cs for better testability and maintainability.

--- src/2048Game/Program.cs
+++ src/2048Game/Program.cs
@@ -3,38 +3,46 @@ using _2048Game.Models.Abstractions;
 using _2048Game.Services;
 using _2048Game.Services.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
 
-var serviceCollection = new ServiceCollection();
+namespace _2048Game;
 
-ConfigureServices();
-
-var serviceProvider = serviceCollection.BuildServiceProvider();
-
-var inputService = serviceProvider.GetRequiredService<InputService>();
-var gameService = serviceProvider.GetRequiredService<IGameService>();
-
-gameService.StartGame();
-var isGameRunning = true;
-while (isGameRunning)
+[ExcludeFromCodeCoverage]
+public sealed class Program
 {
-    isGameRunning = inputService.HandleInput();
-}
+    public static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
 
-return;
+        ConfigureServices(serviceCollection);
 
-void ConfigureServices()
-{
-    serviceCollection.AddSingleton<InputService>();
-    serviceCollection.AddSingleton<IConsoleService, ConsoleService>();
-    serviceCollection.AddSingleton<IGameService, GameService>();
-    serviceCollection.AddSingleton<IRenderService, RenderService>();
-    serviceCollection.AddSingleton<IStorageService, StorageService>();
-    serviceCollection.AddSingleton<ITileControlService, TileControlService>();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
 
-    serviceCollection.AddSingleton<IBoard>(provider =>
-        provider.GetRequiredService<IStorageService>().LoadGame().Item1 ?? new Board()
-    );
-    serviceCollection.AddSingleton<ScoreBoard>(provider =>
-        provider.GetRequiredService<IStorageService>().LoadGame().Item2 ?? new ScoreBoard()
-    );
+        var inputService = serviceProvider.GetRequiredService<InputService>();
+        var gameService = serviceProvider.GetRequiredService<IGameService>();
+
+        gameService.StartGame();
+        var isGameRunning = true;
+        while (isGameRunning)
+        {
+            isGameRunning = inputService.HandleInput();
+        }
+    }
+
+    private static void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<InputService>();
+        serviceCollection.AddSingleton<IConsoleService, ConsoleService>();
+        serviceCollection.AddSingleton<IGameService, GameService>();
+        serviceCollection.AddSingleton<IRenderService, RenderService>();
+        serviceCollection.AddSingleton<IStorageService, StorageService>();
+        serviceCollection.AddSingleton<ITileControlService, TileControlService>();
+
+        serviceCollection.AddSingleton<IBoard>(provider =>
+            provider.GetRequiredService<IStorageService>().LoadGame().Item1 ?? new Board()
+        );
+        serviceCollection.AddSingleton<ScoreBoard>(provider =>
+            provider.GetRequiredService<IStorageService>().LoadGame().Item2 ?? new ScoreBoard()
+        );
+    }
 }
